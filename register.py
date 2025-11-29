270295bbf942409d5ed1cfbaad0cd68197d56027
0x3-lua <93447100+0x3-lua@users.noreply.github.com>
2025-11-29 12:38:02 -0800

    Update register.py

--- register.py
+++ register.py
@@ -1,3 +1,13 @@
+# TESTS:
+# Running Python:                             OK
+# Running sitk.Show :                         OK
+# Running sitk.Show with volumetric:          OK convert_file
+# Running up to HistogramMatchingImageFilter: OK
+# Running DemonsRegistrationFilter:           CRASH
+#   No explicit reason. Running function attempt register
+#   with patient 1 and patient 2 (p1 & p2) on demons regular
+#   and p1 and p1 on demons symmetric both crash.
+
 import SimpleITK as sitk
 
 #export SITK_SHOW_COMMAND=~/Downloads/fiji-latest-linux64-jdk/Fiji/fiji
@@ -11,26 +21,28 @@ def convert_file(path):
 
 	return reader.Execute()
 
-
 def attempt_register(fixed, moving): 
 	img_fixed = convert_file(fixed)
 	img_moving = convert_file(moving)
 
-	#print(img_fixed.GetPixelID()) # 4
+	#i_f_pixel_id = img_fixed.GetPixelID()
+	#print(i_f_pixel_id in (sitk.sitkUInt8, sitk.sitkInt8))
+	# is false
 
 	matcher = sitk.HistogramMatchingImageFilter()
 	matcher.SetNumberOfHistogramLevels(1024)
 	matcher.SetNumberOfMatchPoints(7)
 	matcher.ThresholdAtMeanIntensityOn()
 
-	img_moving = matcher.Execute(img_moving, img_fixed)
+	img_matcher = matcher.Execute(img_moving, img_fixed)
 	
 	#apply demons
-	demons = sitk.FastSymmetricForcesDemonsRegistrationFilter()
-	demons.SetNumberOfIterations(5)
+	demons = sitk.DemonsRegistrationFilter()
+	demons.SetNumberOfIterations(1)
 	demons.SetStandardDeviations(1.0)
 
-	#displacement_field = demons.Execute(img_fixed, img_moving)
+	#crashed
+	displacement_field = demons.Execute(img_fixed, img_matcher)
 	print("check")
 
 patient1 = "/home/a/main/Dataset/Data/Patient1"
